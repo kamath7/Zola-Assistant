caa06902d8582772fc4e0269a17a5b1eff667ee5
kamath7 <adithyakamath96@gmail.com>
2022-02-03 01:35:45 +0530

    Initial engine setup

--- mark-1/app.py
+++ mark-1/app.py
@@ -1,1 +1,19 @@
-print("Hello!")
+from sqlalchemy import engine
+import pyttsx3
+from decouple import config 
+
+username = "LordKams"
+botname = "Zola"
+
+engine = pyttsx3.init('sapi5')
+engine.setProperty('rate',183)
+engine.setProperty('volume',1.0)
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[0].id)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+speak("Welcome {0}! How are you today?".format(username))
